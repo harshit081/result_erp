195b2bfd0d52e684c06dffae46a762bff44f44fa
Divyansh Goel <divyanshgoel2004@gmail.com>
2024-09-25 22:10:57 +0530

    Aadhar verification

--- server/src/controller/controller.js
+++ server/src/controller/controller.js
@@ -1,71 +1,79 @@
-const studentService = require('../service/service.js');
+const studentService = require("../service/service.js");
 
 const fetchSemester = async (req, res) => {
-  try {
-    const { roll_number, acad_year } = req.query;
-    console.log("fetchSemester")
-    const semesters = await studentService.fetchSemesters(roll_number, acad_year);
-    console.log("fetchSemester2")
-    res.json(semesters);
-  } catch (e) {
-    res.status(500).json({ message: e.message });
-  }
+	try {
+		const { roll_number, acad_year } = req.query;
+		console.log("fetchSemester");
+		const semesters = await studentService.fetchSemesters(
+			roll_number,
+			acad_year
+		);
+		console.log("fetchSemester2");
+		res.json(semesters);
+	} catch (e) {
+		res.status(500).json({ message: e.message });
+	}
 };
 
 const fetchAcadYear = async (req, res) => {
-  try {
-    const { roll_number } = req.query;
-    const years = await studentService.fetchAcadYears(roll_number);
-    res.json(years);
-  } catch (e) {
-    res.status(500).json({ message: e.message });
-  }
+	try {
+		const { roll_number } = req.query;
+		const years = await studentService.fetchAcadYears(roll_number);
+		res.json(years);
+	} catch (e) {
+		res.status(500).json({ message: e.message });
+	}
 };
 
 const fetchResult = async (req, res) => {
-  try {
-    const { roll_number, semester, acad_year } = req.query;
-    const result = await studentService.fetchResult(roll_number, semester, acad_year);
-    res.json(result);
-  } catch (e) {
-    res.status(500).json({ message: "Error finding data" });
-  }
+	try {
+		const { roll_number, semester, acad_year, aadhar } = req.query;
+		const result = await studentService.fetchResult(
+			roll_number,
+			semester,
+			acad_year,
+			aadhar
+		);
+		res.json(result);
+	} catch (e) {
+		res.status(500).json({ message: "Error finding data" });
+	}
 };
 
 const pushData = async (req, res) => {
-  try {
-    await studentService.pushData(req.body);
-    res.status(200).json({ message: "Data pushed successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Error pushing data" });
-  }
+	try {
+		await studentService.pushData(req.body);
+		res.status(200).json({ message: "Data pushed successfully" });
+	} catch (error) {
+		res.status(500).json({ message: "Error pushing data" });
+	}
 };
 
 const blockResult = async (req, res) => {
-  try {
-    const { roll_number, block_result } = req.body;
-    await studentService.blockResult(roll_number, block_result);
-    res.status(200).json({ message: "Blocked result updated successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Error updating blocked result" });
-  }
+	try {
+		const { roll_number, block_result } = req.body;
+		await studentService.blockResult(roll_number, block_result);
+		res.status(200).json({ message: "Blocked result updated successfully" });
+	} catch (error) {
+		res.status(500).json({ message: "Error updating blocked result" });
+	}
 };
 
 const unblockResult = async (req, res) => {
-  try {
-    const { roll_number, unblock_result } = req.body;
-    await studentService.unblockResult(roll_number, unblock_result);
-    res.status(200).json({ message: "Unblocked result updated successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Error updating unblocked result" });
-  }
+	try {
+		const { roll_number, unblock_result } = req.body;
+		await studentService.unblockResult(roll_number, unblock_result);
+		res.status(200).json({ message: "Unblocked result updated successfully" });
+	} catch (error) {
+		res.status(500).json({ message: "Error updating unblocked result" });
+	}
 };
 
 module.exports = {
-  fetchSemester,
-  fetchAcadYear,
-  fetchResult,
-  pushData,
-  blockResult,
-  unblockResult
+	fetchSemester,
+	fetchAcadYear,
+	fetchResult,
+	pushData,
+	blockResult,
+	unblockResult,
 };
--- server/src/service/service.js
+++ server/src/service/service.js
@@ -1,72 +1,151 @@
 // service.js
-const pool = require('../db.js');
-const queries = require('../query/query.js');
-const { arrayDifference } = require('../utils/utils.js');  // Move utility functions to a separate file
+const pool = require("../db.js");
+const queries = require("../query/query.js");
+const { arrayDifference } = require("../utils/utils.js"); // Move utility functions to a separate file
 
 const fetchSemesters = async (roll_number, acad_year) => {
-  const result1 = await pool.query(queries.fetchSemesters, [roll_number, acad_year]);
-  const result2 = await pool.query(queries.fetchBlockedResult, [roll_number]);
-  const semesters = result1.rows.map((row) => parseInt(row.semester));
-  const availableSemesters = arrayDifference(semesters, result2.rows[0]["blocked_result"]);
-  return availableSemesters;
+	const result1 = await pool.query(queries.fetchSemesters, [
+		roll_number,
+		acad_year,
+	]);
+	const result2 = await pool.query(queries.fetchBlockedResult, [roll_number]);
+	const semesters = result1.rows.map((row) => parseInt(row.semester));
+	const availableSemesters = arrayDifference(
+		semesters,
+		result2.rows[0]["blocked_result"]
+	);
+	return availableSemesters;
 };
 
 const fetchAcadYears = async (roll_number) => {
-  const result = await pool.query(queries.fetchAcadYears, [roll_number]);
-  return result.rows.map((row) => row.acad_year);
+	const result = await pool.query(queries.fetchAcadYears, [roll_number]);
+	return result.rows.map((row) => row.acad_year);
 };
 
-const fetchResult = async (roll_number, semester, acad_year) => {
-  const batch = await pool.query(queries.fetchBatch, [roll_number]);
-  const result = await pool.query(queries.fetchResult, [
-    roll_number,
-    batch.rows[0].batch,
-    acad_year,
-    semester,
-  ]);
+const fetchResult = async (roll_number, semester, acad_year, aadhar) => {
+	const batch = await pool.query(queries.fetchBatch, [roll_number]);
+	const result = await pool.query(queries.fetchResult, [
+		roll_number,
+		batch.rows[0].batch,
+		acad_year,
+		semester,
+		aadhar,
+	]);
 
-  const structuredResults = {};
+	const structuredResults = {};
 
-  result.rows.forEach(row => {
-    const { rollno, name, mother, father, guardian, abc, prog, campus, batch, semester, course_code, course_name, credit, marks, month_year } = row;
-    if (!structuredResults[rollno]) {
-      structuredResults[rollno] = { roll_no: rollno, name, father, mother, guardian, abc, prog, campus, batch, semesters: {} };
-    }
-    if (!structuredResults[rollno].semesters[semester]) {
-      structuredResults[rollno].semesters[semester] = { semester, courses: [] };
-    }
-    structuredResults[rollno].semesters[semester].courses.push({ course_code, course_name, credit, marks, month_year });
-  });
+	result.rows.forEach((row) => {
+		const {
+			rollno,
+			name,
+			mother,
+			father,
+			guardian,
+			abc,
+			prog,
+			campus,
+			batch,
+			semester,
+			course_code,
+			course_name,
+			credit,
+			marks,
+			month_year,
+		} = row;
+		if (!structuredResults[rollno]) {
+			structuredResults[rollno] = {
+				roll_no: rollno,
+				name,
+				father,
+				mother,
+				guardian,
+				abc,
+				prog,
+				campus,
+				batch,
+				semesters: {},
+			};
+		}
+		if (!structuredResults[rollno].semesters[semester]) {
+			structuredResults[rollno].semesters[semester] = { semester, courses: [] };
+		}
+		structuredResults[rollno].semesters[semester].courses.push({
+			course_code,
+			course_name,
+			credit,
+			marks,
+			month_year,
+		});
+	});
 
-  return Object.values(structuredResults).map(student => ({
-    ...student,
-    semesters: Object.values(student.semesters)
-  }))[0];
+	return Object.values(structuredResults).map((student) => ({
+		...student,
+		semesters: Object.values(student.semesters),
+	}))[0];
 };
 
 const pushData = async (data) => {
-  const { roll_number, name, program, campus, batch, acad_year, semester, course_code, credit, course_name, marks, month_year } = data;
-  const blocked_result = [0];
-  await pool.query(queries.insertStudent, [roll_number, name, program, campus, batch, JSON.stringify(blocked_result)]);
-  await pool.query(queries.insertCourse, [course_code, credit, course_name]);
-  await pool.query(queries.insertResult, [roll_number, semester, acad_year, course_code, marks, month_year]);
+	const {
+		roll_number,
+		name,
+		program,
+		campus,
+		batch,
+		acad_year,
+		semester,
+		course_code,
+		credit,
+		course_name,
+		marks,
+		month_year,
+	} = data;
+	const blocked_result = [0];
+	await pool.query(queries.insertStudent, [
+		roll_number,
+		name,
+		program,
+		campus,
+		batch,
+		JSON.stringify(blocked_result),
+	]);
+	await pool.query(queries.insertCourse, [course_code, credit, course_name]);
+	await pool.query(queries.insertResult, [
+		roll_number,
+		semester,
+		acad_year,
+		course_code,
+		marks,
+		month_year,
+	]);
 };
 
 const blockResult = async (roll_number, block_result) => {
-  await pool.query(queries.updateBlockedResult, [JSON.stringify(block_result[0] == null ? [0] : block_result), roll_number]);
+	await pool.query(queries.updateBlockedResult, [
+		JSON.stringify(block_result[0] == null ? [0] : block_result),
+		roll_number,
+	]);
 };
 
 const unblockResult = async (roll_number, unblock_result) => {
-  const fetchBlocked = await pool.query(`SELECT blocked_result FROM studentinfo WHERE roll_no = $1`, [roll_number]);
-  const blocked_result = arrayDifference(fetchBlocked.rows[0]["blocked_result"], unblock_result);
-  await pool.query(queries.updateBlockedResult, [JSON.stringify(blocked_result), roll_number]);
+	const fetchBlocked = await pool.query(
+		`SELECT blocked_result FROM studentinfo WHERE roll_no = $1`,
+		[roll_number]
+	);
+	const blocked_result = arrayDifference(
+		fetchBlocked.rows[0]["blocked_result"],
+		unblock_result
+	);
+	await pool.query(queries.updateBlockedResult, [
+		JSON.stringify(blocked_result),
+		roll_number,
+	]);
 };
 
 module.exports = {
-  fetchSemesters,
-  fetchAcadYears,
-  fetchResult,
-  pushData,
-  blockResult,
-  unblockResult,
+	fetchSemesters,
+	fetchAcadYears,
+	fetchResult,
+	pushData,
+	blockResult,
+	unblockResult,
 };
