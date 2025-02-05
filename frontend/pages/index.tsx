f079eeac03cfd0edb0ab7f8e9c4ddce5c83cc00d
harshit081 <tharshit9753@gmail.com>
2025-02-05 19:49:58 +0530

    fixed result formating in indexjs

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -33,8 +33,7 @@ const commonButtonClass =
   "px-10 py-4 rounded-xl font-bold transition duration-300 shadow-md transform hover:scale-105";
 const commonContainerClass =
   "flex flex-col items-center min-h-screen min-w-full p-6 pt-16";
-const commonTableClass =
-  "border border-black text-[11px] p-[6px] flex justify-center";
+const commonTableClass = "border !border-black text-[11px] p-[2px] ";
 
 const StudentDetails = () => {
   const myRef = useRef<HTMLDivElement>(null);
@@ -68,15 +67,16 @@ const StudentDetails = () => {
           {result.abc && (
             <div className="font-bold text-sm mt-0">ABC ID : {result.abc}</div>
           )}
-          <div className="flex flex-col md:flex-row w-full pt-10">
-            <div className="flex w-full md:w-1/4 justify-center">
+          <div className="flex w-full pt-1">
+            <div className="flex w-1/5 items-center">
               <img
                 src="/dseulogo.png"
                 alt="DSEU-LOGO"
-                className="w-[50%] md:w-[34%] h-auto"
+                className="w-[50%] h-auto"
               />
             </div>
-            <div className="w-full md:w-3/4">
+
+            <div>
               <div className="text-center flex flex-col mx-auto p-1 text-[#0072B9]">
                 <div className="text-dseublue text-xl font-extrabold font-mono">
                   दिल्ली कौशल एवं उद्यमिता विश्वविद्यालय
@@ -89,7 +89,7 @@ const StudentDetails = () => {
                   Act 04 of 2020)
                 </div>
               </div>
-              <div className="text-center flex flex-col mx-auto">
+              <div className="text-center flex flex-col mx-auto my-0">
                 <div className="text-lg font-serif p-1">
                   Grade sheet of EoSE of{" "}
                   <span className="font-bold font-sans">June-2024</span>
@@ -102,10 +102,10 @@ const StudentDetails = () => {
             </div>
           </div>
 
-          <div className="border-[1px] px-3 mx-1 pt-4">
+          <div className="border px-8 pt-4">
             <div className="student-info mb-4 flex justify-center">
-              <div className="w-full md:w-[80%]">
-                <div className="flex flex-col md:flex-row justify-between">
+              <div className="w-[100%]">
+                <div className="flex justify-between">
                   <div className="flex-col">
                     <div className="p-0">
                       Student Name:{" "}
@@ -153,28 +153,42 @@ const StudentDetails = () => {
               <table className="w-full md:w-[90%] border border-black border-collapse">
                 {/* Header */}
                 <thead>
-                  <tr>
-                    <th className={`${commonTableClass} w-[10%] font-bold`}>
+                  <tr className="border-b !border-black">
+                    <td
+                      className={`${commonTableClass} w-[7%] font-bold text-center`}
+                    >
                       S.No
-                    </th>
-                    <th className={`${commonTableClass} w-[15%] font-bold`}>
+                    </td>
+                    <td
+                      className={`${commonTableClass} w-[15%] font-bold text-center`}
+                    >
                       Course Code
-                    </th>
-                    <th className={`${commonTableClass} w-[35%] font-bold`}>
+                    </td>
+                    <td
+                      className={`${commonTableClass} w-[33%] font-bold text-center`}
+                    >
                       Course Name
-                    </th>
-                    <th className={`${commonTableClass} w-[10%] font-bold`}>
+                    </td>
+                    <td
+                      className={`${commonTableClass} w-[7%] font-bold text-center`}
+                    >
                       Credit
-                    </th>
-                    <th className={`${commonTableClass} w-[10%] font-bold`}>
-                      Credit Earned
-                    </th>
-                    <th className={`${commonTableClass} w-[10%] font-bold`}>
+                    </td>
+                    <td
+                      className={`${commonTableClass} w-[15%] font-bold text-center`}
+                    >
+                      Credit Earned (Cᵢ)
+                    </td>
+                    <td
+                      className={`${commonTableClass} w-[10%] font-bold text-center`}
+                    >
                       Grade
-                    </th>
-                    <th className={`${commonTableClass} w-[10%] font-bold`}>
-                      Grade Point
-                    </th>
+                    </td>
+                    <td
+                      className={`${commonTableClass} w-[13%] font-bold text-center`}
+                    >
+                      Grade Point (Pᵢ)
+                    </td>
                   </tr>
                 </thead>
 
@@ -184,41 +198,41 @@ const StudentDetails = () => {
                     (mark: Mark, index: number) => (
                       <tr key={index}>
                         <td
-                          className={`${commonTableClass} w-[10%] text-[10px]`}
+                          className={`${commonTableClass} w-[10%] text-[10px] text-center`}
                         >
                           {index + 1}
                         </td>
                         <td
-                          className={`${commonTableClass} w-[15%] text-[10px]`}
+                          className={`${commonTableClass} w-[15%] text-[10px] `}
                         >
                           {mark.course_code}
                         </td>
                         <td
-                          className={`${commonTableClass} w-[35%] text-[10px]`}
+                          className={`${commonTableClass} w-[30%] text-[10px] `}
                         >
                           {mark.course_name}
                         </td>
                         <td
-                          className={`${commonTableClass} w-[10%] text-[10px]`}
+                          className={`${commonTableClass} w-[7%] text-[10px] text-center`}
                         >
                           {mark.credit}
                         </td>
                         <td
-                          className={`${commonTableClass} w-[10%] text-[10px]`}
+                          className={`${commonTableClass} w-[15%] text-[10px] text-center`}
                         >
                           {eval_gp(mark?.marks) >= 4
                             ? mark.credit
                             : eval_gp(mark?.marks) == -1
-                            ? "-"
-                            : 0}
+                              ? "-"
+                              : 0}
                         </td>
                         <td
-                          className={`${commonTableClass} w-[10%] text-[10px]`}
+                          className={`${commonTableClass} w-[10%] text-[10px] text-center`}
                         >
                           {eval_grade(mark?.marks, mark?.credit)}
                         </td>
                         <td
-                          className={`${commonTableClass} w-[10%] text-[10px]`}
+                          className={`${commonTableClass} w-[13%] text-[10px] text-center`}
                         >
                           {mark.credit != 0 ? eval_gp(mark?.marks) : "-"}
                         </td>
@@ -229,82 +243,56 @@ const StudentDetails = () => {
               </table>
             </div>
 
-            <div className="summary-table w-full flex justify-center">
-              <div className="flex flex-col w-full md:w-[90%] border border-black border-collapse">
-                <div className="flex flex-col md:flex-row">
-                  <div
-                    className={`${commonTableClass} w-full md:w-[14.28%] text-[11px] p-2 flex justify-center items-center`}
-                  >
-                    Credits earned in this semester
-                  </div>
-                  <div
-                    className={`${commonTableClass} w-full md:w-[14.28%] text-[11px] p-2 flex justify-center items-center`}
-                  >
-                    Total credits as on date
-                  </div>
-                  <div className="border border-black w-full md:w-[28.56%] flex flex-col">
-                    <div className="text-[11px] p-2 flex justify-center items-center">
-                      SGPA
-                    </div>
-                    <div className="flex justify-evenly">
-                      <div className="border border-t-black border-r-black w-[50%] text-[11px] p-2 flex justify-center items-center">
-                        Earned
-                      </div>
-                      <div className="border border-t-black border-l-black w-[50%] text-[11px] p-2 flex justify-center items-center">
-                        Grade letter
-                      </div>
-                    </div>
-                  </div>
-                  <div className="border border-black w-full md:w-[28.56%] flex flex-col">
-                    <div className="text-[11px] p-2 flex justify-center items-center">
-                      CGPA
-                    </div>
-                    <div className="flex justify-evenly">
-                      <div className="border border-t-black border-r-black flex-1 text-[11px] p-2 flex justify-center items-center">
-                        Earned
-                      </div>
-                      <div className="border border-t-black border-l-black flex-1 text-[11px] p-2 flex justify-center items-center">
-                        Grade letter
-                      </div>
-                    </div>
-                  </div>
-                  <div className="border border-black flex-1 text-[11px] p-2 flex justify-center items-center">
-                    Grading System
-                  </div>
-                </div>
 
-                <div className="flex flex-col md:flex-row">
-                  <div className={`${commonTableClass} flex-1 text-[10px] p-2`}>
-                    {result ? tot_sem_cred(result.semesters[0].courses) : "-"}
-                  </div>
-                  <div className={`${commonTableClass} flex-1 text-[10px] p-2`}>
-                    -
-                  </div>
-                  <div className={`${commonTableClass} flex-1 text-[10px] p-2`}>
-                    {result ? sgpa_calc(result.semesters[0].courses) : "-"}
-                  </div>
-                  <div className={`${commonTableClass} flex-1 text-[10px] p-2`}>
-                    {result
-                      ? sem_grade(sgpa_calc(result.semesters[0].courses))
-                      : "-"}
-                  </div>
-                  <div className={`${commonTableClass} flex-1 text-[10px] p-2`}>
-                    -
-                  </div>
-                  <div className={`${commonTableClass} flex-1 text-[10px] p-2`}>
-                    -
-                  </div>
-                  <div className={`${commonTableClass} flex-1 text-[10px] p-2`}>
-                    ABS
-                  </div>
-                </div>
-              </div>
-            </div>
+<div className="summary-table w-full flex justify-center">
+  <table className="w-[90%] border border-collapse !border-black">
+    {/* Header */}
+    <thead>
+      <tr className="border-b !border-black font-black text-[11px] text-center">
+        <td rowSpan={2} className="border !border-black p-2">Credits Earned in this semester</td>
+        <td rowSpan={2} className="border !border-black p-2">Total Credits earned as on date</td>
+        <td colSpan={2} className="border !border-black p-2">SGPA</td>
+        <td colSpan={2} className="border !border-black p-2">CGPA</td>
+        <td rowSpan={2} className="border !border-black p-2 align-middle">Grading System</td>
+      </tr>
+      <tr className="border-b !border-black font-black text-[11px] text-center">
+        <td className="border !border-black p-2">Earned</td>
+        <td className="border !border-black p-2">Grade Letter</td>
+        <td className="border !border-black p-2">Earned</td>
+        <td className="border !border-black p-2">Grade Letter</td>
+      </tr>
+    </thead>
+    {/* Data */}
+    <tbody>
+      <tr>
+        <td className="border !border-black text-[10px] p-2 text-center font-black">
+          {result ? tot_sem_cred(result.semesters[0].courses) : "-"}
+        </td>
+        <td className="border !border-black text-[10px] p-2 text-center font-black">-</td>
+        <td className="border !border-black text-[10px] p-2 text-center font-black">
+          {result ? sgpa_calc(result.semesters[0].courses) : "-"}
+        </td>
+        <td className="border !border-black text-[10px] p-2 text-center font-black">
+          {result ? sem_grade(sgpa_calc(result.semesters[0].courses)) : "-"}
+        </td>
+        <td className="border !border-black text-[10px] p-2 text-center font-black">-</td>
+        <td className="border !border-black text-[10px] p-2 text-center font-black">-</td>
+        <td className="border !border-black text-[10px] p-2 text-center font-black align-middle">ABS</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+
+
+            <p className="flex w-full place-content-end items-end">
+              Computer Generated Result Grade Sheet
+            </p>
           </div>
         </div>
-        <div className="page-break"></div>
+        {/* <div className="page-break"></div> */}
         <div
-          className="w-full h-screen bg-cover bg-center"
+          className="w-full h-screen bg-cover bg-center page-break"
           style={{ backgroundImage: `url('/moi.jpg')` }}
         ></div>
       </>
@@ -314,9 +302,8 @@ const StudentDetails = () => {
   return (
     <>
       <div
-        className={`${
-          darkMode ? "bg-gray-900 text-gray-100" : "bg-gray-100 text-gray-900"
-        } transition duration-500`}
+        className={`${darkMode ? "bg-gray-900 text-gray-100" : "bg-gray-100 text-gray-900"
+          } transition duration-500`}
         style={{
           backgroundImage: `url('/back.svg')`,
           backgroundRepeat: "no-repeat",
@@ -334,18 +321,16 @@ const StudentDetails = () => {
               className="h-24 w-auto ml-10"
             />
             <div
-              className={`font-[1000] font-serif text-xl md:text-5xl mx-auto ${
-                darkMode ? "text-gray-100" : "text-gray-800"
-              }`}
+              className={`font-[1000] font-serif text-xl md:text-5xl mx-auto ${darkMode ? "text-gray-100" : "text-gray-800"
+                }`}
             >
               R E S U L T
             </div>
             <IconButton
               aria-label="Toggle Dark Mode"
               onClick={handleSwitchChange}
-              className={`transition duration-300 mr-10 ${
-                darkMode ? "text-gray-100" : "text-gray-900"
-              }`}
+              className={`transition duration-300 mr-10 ${darkMode ? "text-gray-100" : "text-gray-900"
+                }`}
             >
               {darkMode ? <Brightness7Icon /> : <Brightness4 />}
             </IconButton>
@@ -356,9 +341,8 @@ const StudentDetails = () => {
             component="form"
             noValidate
             autoComplete="off"
-            className={`p-10 bg-white bg-opacity-30 border border-gray-500 rounded-[40px] flex flex-col justify-between items-center gap-8 w-full max-w-[90%] md:max-w-[75%] lg:max-w-[60%] mt-5 ${
-              darkMode ? "bg-gray-800 bg-opacity-30" : "bg-white bg-opacity-30"
-            }`}
+            className={`p-10 bg-white bg-opacity-30 border border-gray-500 rounded-[40px] flex flex-col justify-between items-center gap-8 w-full max-w-[90%] md:max-w-[75%] lg:max-w-[60%] mt-5 ${darkMode ? "bg-gray-800 bg-opacity-30" : "bg-white bg-opacity-30"
+              }`}
             style={{
               backdropFilter: "blur(2px)",
             }}
@@ -380,9 +364,8 @@ const StudentDetails = () => {
               }
               fullWidth
               margin="normal"
-              className={`${
-                darkMode ? "bg-gray-300" : "bg-gray-50"
-              } ${commonInputClass}`}
+              className={`${darkMode ? "bg-gray-300" : "bg-gray-50"
+                } ${commonInputClass}`}
               InputProps={{
                 sx: {
                   "& input": {
@@ -408,9 +391,8 @@ const StudentDetails = () => {
               fullWidth
               margin="normal"
               inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-              className={`${
-                darkMode ? "bg-gray-300" : "bg-gray-50"
-              } ${commonInputClass}`}
+              className={`${darkMode ? "bg-gray-300" : "bg-gray-50"
+                } ${commonInputClass}`}
               InputProps={{
                 sx: {
                   "& input": {
@@ -426,9 +408,8 @@ const StudentDetails = () => {
             <FormControl
               fullWidth
               margin="normal"
-              className={`${
-                darkMode ? "bg-gray-300" : "bg-gray-50"
-              } ${commonSelectClass}`}
+              className={`${darkMode ? "bg-gray-300" : "bg-gray-50"
+                } ${commonSelectClass}`}
             >
               <InputLabel id="academic-year-label">ACADEMIC YEAR</InputLabel>
               <Select
@@ -509,11 +490,10 @@ const StudentDetails = () => {
                   result
                 )
               }
-              className={`${commonButtonClass} ${
-                darkMode
+              className={`${commonButtonClass} ${darkMode
                   ? "bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800"
                   : "bg-gradient-to-r from-blue-600 to-blue-800 hover:from-blue-700 hover:to-blue-900"
-              } text-white`}
+                } text-white`}
             >
               Submit
             </Button>
@@ -533,11 +513,10 @@ const StudentDetails = () => {
                 />
                 <div
                   ref={componentRef}
-                  className={`w-full h-full rounded-xl shadow-lg mt-1 p-8 ${
-                    darkMode
+                  className={`w-full h-full rounded-xl shadow-lg mt-1 p-8 ${darkMode
                       ? "bg-white text-gray-900"
                       : "bg-white text-gray-900"
-                  }`}
+                    }`}
                 >
                   {renderStudentResults()}
                 </div>
@@ -545,15 +524,14 @@ const StudentDetails = () => {
             </>
           ) : (
             <div
-              className={`mt-10 font-semibold text-lg ${
-                valid ? "text-gray-500" : "text-red-900"
-              }`}
+              className={`mt-10 font-semibold text-lg ${valid ? "text-gray-500" : "text-red-900"
+                }`}
             >
               {!valid
                 ? "Credentials Mismatch"
                 : semesters[0] === "0"
-                ? "Result Not Available Kindly Contact Campus Director"
-                : "Nothing to show"}
+                  ? "Result Not Available Kindly Contact Campus Director"
+                  : "Nothing to show"}
             </div>
           )}
         </Container>
